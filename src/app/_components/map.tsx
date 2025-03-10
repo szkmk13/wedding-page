ed1c01ca078acadd339e0bfd6da39a5005167230
Szymon Kowalski <szkmk131@gmail.com>
2025-03-10 13:16:46 +0100

    version 1.5.4

--- src/app/_components/map.tsx
+++ src/app/_components/map.tsx
@@ -1,101 +1,15 @@
-/* eslint-disable */
-"use client";
-
-import { useEffect, useRef } from "react";
-import 'leaflet/dist/leaflet.css';
+import "leaflet/dist/leaflet.css";
 
 export default function Map() {
-  const mapRef = useRef(null);
-  const mapInstanceRef = useRef(null);
-  const mapInitializedRef = useRef(false);
-
-  useEffect(() => {
-    // Jeśli mapa już została zainicjalizowana, nie inicjalizuj ponownie
-    if (mapInitializedRef.current) return;
-
-    // Asynchroniczne ładowanie Leaflet
-    const initializeMap = async () => {
-      if (typeof window === "undefined") return;
-
-      try {
-        // Importuj CSS i bibliotekę Leaflet
-        const L = await import("leaflet");
-
-        // Konfiguracja domyślnej ikony
-        const DefaultIcon = L.icon({
-          iconUrl:
-            "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png",
-          shadowUrl:
-            "https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png",
-          iconSize: [25, 41],
-          iconAnchor: [12, 41],
-          popupAnchor: [1, -34],
-          shadowSize: [41, 41],
-        });
-        L.Marker.prototype.options.icon = DefaultIcon;
-
-        const churchPosition = {
-          lat: 53.65597270827541,
-          lng: 19.689803565103066,
-        };
-        const venuePosition = {
-          lat: 53.61228235756263,
-          lng: 19.612065854335807,
-        };
-        const centerPosition = {
-          lat: (churchPosition.lat + venuePosition.lat) / 2,
-          lng: (churchPosition.lng + venuePosition.lng) / 2,
-        };
-
-
-        // Sprawdź, czy istnieje już mapa i usuń ją
-        if (mapInstanceRef.current) {
-          mapInstanceRef.current.remove();
-          mapInstanceRef.current = null;
-        }
-
-        // Inicjalizuj mapę tylko jeśli kontener istnieje i mapa nie jest już zainicjalizowana
-        if (mapRef.current && !mapInstanceRef.current) {
-          mapInstanceRef.current = L.map(mapRef.current).setView(
-            centerPosition,
-            12,
-          );
-
-          L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-            attribution: "&copy; OpenStreetMap contributors",
-          }).addTo(mapInstanceRef.current);
-
-          L.marker(churchPosition)
-            .addTo(mapInstanceRef.current)
-            .bindPopup("Kościół - Uroczystość zaślubin");
-
-          L.marker(venuePosition)
-            .addTo(mapInstanceRef.current)
-            .bindPopup("Sala Weselna - Tu się widzimy!");
-
-          // Oznacz mapę jako zainicjalizowaną
-          mapInitializedRef.current = true;
-        }
-      } catch (error) {
-        console.error("Błąd inicjalizacji mapy:", error);
-      }
-    };
-
-    initializeMap();
-
-    // Funkcja czyszcząca przy odmontowaniu komponentu
-    return () => {
-      if (mapInstanceRef.current) {
-        mapInstanceRef.current = null;
-      }
-      mapInitializedRef.current = false;
-    };
-  }, []);
-
+  const key = process.env.NEXT_PUBLIC_GOOGLE_API_KEY;
   return (
-    <div
-      ref={mapRef}
-      style={{ height: "400px", width: "100%", position: "relative" }}
-    />
+    <iframe
+      src="https://www.google.com/maps/embed?pb=!1m26!1m12!1m3!1d37851.43404517658!2d19.611485598361487!3d53.634228798072364!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m11!3e0!4m5!1s0x471d6823a1e507cf%3A0x5ba11885bbcf9543!2zS2_Fm2Npw7PFgiBSenltc2tva2F0b2xpY2tpIHB3LiBUcsOzamN5IFByemVuYWrFm3dpxJl0c3plaiwgRnJlZG5vd3ksIDE0LTIwMA!3m2!1d53.6558935!2d19.6897745!4m3!3m2!1d53.6134012!2d19.613871!5e0!3m2!1spl!2spl!4v1741608593730!5m2!1spl!2spl"
+      
+      className="h-64 sm:h-96 w-full"
+      allowFullScreen={true}
+      loading="lazy"
+      referrerPolicy="no-referrer-when-downgrade"
+    ></iframe>
   );
 }
--- src/components/directions.tsx
+++ src/components/directions.tsx
@@ -4,24 +4,28 @@ import { MapPin, Users, Car, ChevronDown } from "lucide-react";
 import dynamic from "next/dynamic";
 
 import Link from "next/link";
-const Map = dynamic(() => import("../app/_components/map"), {
-  ssr: false,
-  loading: () => <div>Ładowanie...</div>,
-});
-
+// const Map = dynamic(() => import("../app/_components/map"), {
+//   ssr: false,
+// });
 export default function DirectionsMap() {
   const [showDropdown, setShowDropdown] = useState(false);
-
   return (
     <div className="w-full bg-white/90 px-4 py-8 backdrop-blur-sm sm:px-6 sm:py-12">
       <div className="mx-auto max-w-3xl">
         <h2 className="mb-8 text-center text-3xl font-bold tracking-tight sm:text-4xl">
           Jak dojechać?
         </h2>
+        
         <div className="mb-8 overflow-hidden rounded-lg bg-beige/90 shadow-lg">
           <div className="h-64 sm:h-96">
-            <Map />
-          </div>
+          <iframe
+      src="https://www.google.com/maps/embed?pb=!1m26!1m12!1m3!1d37851.43404517658!2d19.611485598361487!3d53.634228798072364!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m11!3e0!4m5!1s0x471d6823a1e507cf%3A0x5ba11885bbcf9543!2zS2_Fm2Npw7PFgiBSenltc2tva2F0b2xpY2tpIHB3LiBUcsOzamN5IFByemVuYWrFm3dpxJl0c3plaiwgRnJlZG5vd3ksIDE0LTIwMA!3m2!1d53.6558935!2d19.6897745!4m3!3m2!1d53.6134012!2d19.613871!5e0!3m2!1spl!2spl!4v1741608593730!5m2!1spl!2spl"
+      
+      className="h-64 sm:h-96 w-full"
+      allowFullScreen={true}
+      loading="lazy"
+      referrerPolicy="no-referrer-when-downgrade"
+    ></iframe>          </div>
         </div>
 
         <div className="flex flex-col justify-center gap-4 sm:flex-row">
